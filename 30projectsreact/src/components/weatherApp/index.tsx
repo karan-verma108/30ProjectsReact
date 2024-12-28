f2333e45a2d0318120e2b849fd1dacdd7dfcf626
karan <karanverma201411@gmail.com>
2024-12-28 15:31:33 +0530

    react : added backgrounds to app

--- 30projectsreact/src/components/weatherApp/index.tsx
+++ 30projectsreact/src/components/weatherApp/index.tsx
@@ -17,6 +17,40 @@ export default function WeatherApp(): JSX.Element {
     React.Dispatch<React.SetStateAction<boolean>>
   ] = useState<boolean>(false);
 
+  const [isDropdownMenuOpen, setIsDropdownMenuOpen]: [
+    boolean,
+    React.Dispatch<React.SetStateAction<boolean>>
+  ] = useState<boolean>(false);
+
+  const [currentBackground, setCurrentBackground]: [
+    string,
+    React.Dispatch<React.SetStateAction<string>>
+  ] = useState<string>('');
+
+  const tempFromApi: string = (data?.main?.temp - 273.15).toFixed(2);
+  let tempInCelcius: number = Number(tempFromApi);
+
+  const handleCurrentBackground = (): void => {
+    tempInCelcius = Number(Math.round(tempInCelcius));
+
+    const currentDay: Date = new Date();
+    const currentHour: number = currentDay.getHours();
+
+    if (tempInCelcius > 20 && currentHour < 12) {
+      setCurrentBackground('normalDay');
+    } else if (tempInCelcius < 20 && currentHour < 20) {
+      setCurrentBackground('coldDay');
+    } else if (tempInCelcius > 20 && currentHour > 12 && currentHour <= 16) {
+      setCurrentBackground('hotDay');
+    } else if (currentHour >= 16 && currentHour <= 18) {
+      setCurrentBackground('evening');
+    } else if (currentHour >= 18 && currentHour <= 23) {
+      setCurrentBackground('night');
+    } else {
+      setCurrentBackground('');
+    }
+  };
+
   const getWeatherDetails = () => {
     return new Promise((resolve, reject) => {
       if (navigator.geolocation) {
@@ -78,18 +112,84 @@ export default function WeatherApp(): JSX.Element {
     return `${hours}:${minutes}:${seconds} ${meridiem}`;
   };
 
+  const getDayAndTime = () => {
+    const date = new Date();
+    const todayDate = date.toLocaleDateString();
+    const day = date.getDay();
+    let dayResult;
+
+    switch (day) {
+      case 1:
+        dayResult = 'Monday';
+        break;
+
+      case 2:
+        dayResult = 'Tuesday';
+        break;
+      case 3:
+        dayResult = 'Wednesday';
+        break;
+      case 4:
+        dayResult = 'Thursday';
+        break;
+      case 5:
+        dayResult = 'Friday';
+        break;
+      case 6:
+        dayResult = 'Saturday';
+        break;
+      case 7:
+        dayResult = 'Sunday';
+        break;
+
+      default:
+        dayResult = '';
+    }
+
+    return { todayDate, dayResult };
+  };
+
+  const { todayDate, dayResult }: { todayDate: string; dayResult: string } =
+    getDayAndTime();
+
   const bgColor: string | null = localStorage.getItem('bgColor');
 
-  setInterval(() => {
-    const timeResult = getCurrentTime();
+  const dropdownMenu: HTMLElement | null =
+    document.getElementById('dropdown-menu');
+  const searchInput: null | HTMLInputElement =
+    document.getElementById('search-input');
+
+  searchInput?.addEventListener('input', () => {
+    const searchTerm: string = searchInput?.value?.toLowerCase();
+    const items: NodeListOf<HTMLAnchorElement> | undefined =
+      dropdownMenu?.querySelectorAll('a');
+
+    items?.forEach((item) => {
+      const text = item?.textContent?.toLowerCase();
+      if (text?.includes(searchTerm)) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  });
+
+  useEffect(() => {
+    const timeResult: string = getCurrentTime();
     setTime(timeResult);
-  }, 1000);
+  }, []);
 
   useEffect(() => {
     getWeatherDetails()
       .then(handleFetchData)
       .catch((err) => console.log('err', err));
   }, []);
+
+  useEffect(() => {
+    if (!isNaN(tempInCelcius)) {
+      handleCurrentBackground();
+    }
+  }, [tempInCelcius]);
 
   return (
     <div
@@ -99,11 +199,11 @@ export default function WeatherApp(): JSX.Element {
     >
       {data && data !== undefined && (
         <div
-          className={`xl:w-1/4 w-11/12 h-3/4 rounded-lg ${
+          className={`xl:w-1/4 w-full flex flex-col gap-28 h-screen rounded-lg mx-auto bg-${currentBackground} bg-no-repeat bg-cover ${
             bgColor ?? ''
           } p-2.5 shadow-2xl`}
         >
-          <div className='flex justify-between h-1/3'>
+          <div className='flex justify-between'>
             <div>
               <p
                 className={`${
@@ -133,16 +233,75 @@ export default function WeatherApp(): JSX.Element {
               )}
             </div>
           </div>
+          <div className='flex items-center justify-center'>
+            <div className='relative group'>
+              <button
+                className='inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500'
+                onClick={() => setIsDropdownMenuOpen(!isDropdownMenuOpen)}
+              >
+                <span className='mr-2'>{data?.name}</span>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='w-5 h-5 ml-2 -mr-1'
+                  viewBox='0 0 20 20'
+                  fill='currentColor'
+                  aria-hidden='true'
+                >
+                  <path
+                    fillRule='evenodd'
+                    d='M6.293 9.293a1 1 0 011.414 0L10 11.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z'
+                    clipRule='evenodd'
+                  />
+                </svg>
+              </button>
+              {isDropdownMenuOpen && (
+                <div className='absolute right-0 mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 p-1 space-y-1'>
+                  <input
+                    id='search-input'
+                    className='block w-full px-4 py-2 text-gray-800 border rounded-md  border-gray-300 focus:outline-none'
+                    type='text'
+                    placeholder='Search items'
+                    autoComplete='off'
+                  />
+                  <a
+                    href='#'
+                    className='block px-4 py-2 text-gray-700 hover:bg-gray-100 active:bg-blue-100 cursor-pointer rounded-md'
+                  >
+                    Uppercase
+                  </a>
+                  <a
+                    href='#'
+                    className='block px-4 py-2 text-gray-700 hover:bg-gray-100 active:bg-blue-100 cursor-pointer rounded-md'
+                  >
+                    Lowercase
+                  </a>
+                  <a
+                    href='#'
+                    className='block px-4 py-2 text-gray-700 hover:bg-gray-100 active:bg-blue-100 cursor-pointer rounded-md'
+                  >
+                    Camel Case
+                  </a>
+                  <a
+                    href='#'
+                    className='block px-4 py-2 text-gray-700 hover:bg-gray-100 active:bg-blue-100 cursor-pointer rounded-md'
+                  >
+                    Kebab Case
+                  </a>
+                </div>
+              )}
+            </div>
+          </div>
+
           <div
-            className={`w-full h-1/2 flex flex-col justify-center items-center rounded-xl ${
-              bgColor === 'bg-violet-800' ? 'bg-black' : 'bg-white'
+            className={`w-2/3 h-1/3 flex flex-col text-xl justify-center items-center rounded-xl mx-auto ${
+              bgColor === 'bg-violet-800'
+                ? 'bg-black bg-opacity-50'
+                : 'bg-white'
             } text-2xl`}
           >
-            <p>Your city is : {data?.name}</p>
-            <p>Your country is : {data?.sys?.country}</p>
-            <p>
-              Your temperature is : {(data?.main?.temp - 273.15).toFixed(2)}°C
-            </p>
+            <p>{todayDate}</p>
+            <p>{dayResult}</p>
+            <p>{tempInCelcius}°C</p>
           </div>
         </div>
       )}
--- 30projectsreact/tailwind.config.js
+++ 30projectsreact/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        normalDay:
+          "url('https://img.freepik.com/premium-photo/italian-vineyard-landscape-with-hous_926199-203543.jpg')",
+        coldDay:
+          "url('https://ai.flux-image.com/flux/4ad8f4da-597e-4648-9099-5739e47f4919.jpg')",
+        hotDay:
+          "url('https://st2.depositphotos.com/4164031/8139/i/450/depositphotos_81398650-stock-photo-green-field-under-the-sun.jpg')",
+        evening:
+          "url('https://i.pinimg.com/474x/03/30/4f/03304f4db69facda5fb01bc837750c1f.jpg')",
+        night:
+          "url('https://storage.ko-fi.com/cdn/useruploads/display/59388fac-810b-4777-808c-bf52ad7ffaa8_xstrangee_a_man_and_a_woman_in_a_convertible_car_on_top_of_a__4c49ccc1-4eab-4e46-8f5b-3e369d0b7f15_3.png')",
+      },
+    },
   },
   plugins: [],
 };
