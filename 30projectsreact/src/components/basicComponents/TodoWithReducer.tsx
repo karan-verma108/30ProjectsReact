e8aaa1d4b04f9797ddf7e6138e6ed574a5dc82db
karan <karanverma201411@gmail.com>
2025-03-20 23:02:37 +0530

    react : need to fix save logic

--- 30projectsreact/src/components/basicComponents/TodoWithReducer.tsx
+++ 30projectsreact/src/components/basicComponents/TodoWithReducer.tsx
@@ -5,6 +5,8 @@ export default function TodoWithReducer(): JSX.Element {
   const initialState: InitialStateType = {
     todoItem: '',
     todoItems: [],
+    editTodoId: null,
+    editedTodoValue: '',
   };
 
   const todoReducer = (state: any, action: TodoAction) => {
@@ -15,6 +17,7 @@ export default function TodoWithReducer(): JSX.Element {
               ...state,
               todoItems: [...state.todoItems, action.payload],
               todoItem: '',
+              editTodoId: null,
             }
           : { ...state };
       case 'UPDATE_TODO_ITEM':
@@ -28,6 +31,22 @@ export default function TodoWithReducer(): JSX.Element {
           ),
         };
 
+      case 'EDIT':
+        return { ...state, editTodoId: action.payload };
+
+      case 'HANDLE_EDITED_TODO_ITEM':
+        return { ...state, editedTodoValue: action.payload };
+
+      // case 'SAVE':
+      //   return {
+      //     ...state,
+      //     todoItems: [
+      //       ...state.todoItems,
+      //       state.todoItems.splice(action.payload, 1, state.editedTodoValue),
+      //     ],
+      //     editTodoId: null,
+      //   };
+
       default:
         break;
     }
@@ -36,8 +55,14 @@ export default function TodoWithReducer(): JSX.Element {
   const [state, dispatch]: [InitialStateType, React.Dispatch<TodoAction>] =
     useReducer(todoReducer, initialState);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleTodoItemChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch({ type: 'UPDATE_TODO_ITEM', payload: e.target.value });
+  };
+
+  const handleEditedTodoItemChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    dispatch({ type: 'HANDLE_EDITED_TODO_ITEM', payload: e.target.value });
   };
 
   console.log('state', state);
@@ -53,11 +78,11 @@ export default function TodoWithReducer(): JSX.Element {
           }}
         >
           <input
-            className='border border-slate-300 '
+            className='border border-slate-300'
             type='text'
             placeholder='enter your todo'
             value={state?.todoItem}
-            onChange={handleChange}
+            onChange={handleTodoItemChange}
           />
           <button
             type='submit'
@@ -73,13 +98,43 @@ export default function TodoWithReducer(): JSX.Element {
                 key={`${item}-${index}`}
                 className='flex justify-between items-center bg-amber-100'
               >
-                <p>{item}</p>
-                <button
-                  onClick={() => dispatch({ type: 'DELETE', payload: index })}
-                  className='bg-red-600 text-white rounded-sm py-1 px-2 cursor-pointer'
-                >
-                  Delete
-                </button>
+                <input
+                  value={
+                    state.editTodoId === index ? state.editedTodoValue : item
+                  }
+                  id='item'
+                  name='item'
+                  placeholder={state.editTodoId === index ? item : ''}
+                  autoFocus={state.editTodoId === index}
+                  disabled={state.editTodoId !== index}
+                  onChange={handleEditedTodoItemChange}
+                  className='disabled:cursor-not-allowed disabled:bg-slate-200 focus:scale-105 transition-all duration-200'
+                />
+                {state.editTodoId !== index ? (
+                  <div className='flex gap-2'>
+                    <button
+                      onClick={() =>
+                        dispatch({ type: 'DELETE', payload: index })
+                      }
+                      className='bg-red-600 text-white rounded-sm py-1 px-2 cursor-pointer'
+                    >
+                      Delete
+                    </button>
+                    <button
+                      onClick={() => dispatch({ type: 'EDIT', payload: index })}
+                      className='bg-orange-600 text-white rounded-sm py-1 px-2 cursor-pointer'
+                    >
+                      Edit
+                    </button>
+                  </div>
+                ) : (
+                  <button
+                    onClick={() => dispatch({ type: 'SAVE', payload: index })}
+                    className='bg-green-600 text-white rounded-sm py-1 px-2 cursor-pointer'
+                  >
+                    Save
+                  </button>
+                )}
               </div>
             ))}
         </div>
--- 30projectsreact/types/index.d.ts
+++ 30projectsreact/types/index.d.ts
@@ -8,9 +8,14 @@ export interface NavbarDataType {
 export interface InitialStateType {
   todoItem: string;
   todoItems: string[];
+  editTodoId: null | number;
+  editedTodoValue: string;
 }
 
 export type TodoAction =
   | { type: 'ADD'; payload: string }
   | { type: 'UPDATE_TODO_ITEM'; payload: string }
-  | { type: 'DELETE'; payload: number };
+  | { type: 'DELETE'; payload: number }
+  | { type: 'EDIT'; payload: number }
+  | { type: 'HANDLE_EDITED_TODO_ITEM'; payload: string }
+  | { type: 'SAVE'; payload: number };
